765f151d5b876af4d792c6981ff5c70e5963d8be
fabio <fabioborges14@icloud.com>
2020-12-07 23:12:47 +0000

    Fábio Day 1 and 2

--- Solvers/Wizards/Fabio/Fabio01.cs
+++ Solvers/Wizards/Fabio/Fabio01.cs
@@ -14,12 +14,41 @@ namespace Solvers
 
         public override long SolvePartOne(string[] input)
         {
-            throw new NotImplementedException();
+            int[] inputNum = input.Select(t => Convert.ToInt32(t)).ToArray();
+
+            for (var i = 0; i < inputNum.Length; i++)
+            {
+                var baseValue = inputNum[i];
+                for (var j = i + 1; j < inputNum.Length; j++)
+                {
+                    var otherValue = inputNum[j];
+                    if (baseValue + otherValue == 2020)
+                        return baseValue * otherValue;
+                }
+            }
+            return -1;
         }
 
         public override long SolvePartTwo(string[] input)
         {
-            throw new NotImplementedException();
+            int[] inputNum = input.Select(t => Convert.ToInt32(t)).ToArray();
+
+            for (var i = 0; i < inputNum.Length; i++)
+            {
+                var baseValue = inputNum[i];
+                for (var j = i + 1; j < inputNum.Length; j++)
+                {
+                    var secondValue = inputNum[j];
+                    for (var k = j + 1; k < inputNum.Length; k++)
+                    {
+                        var thirdValue = inputNum[k];
+                        if (baseValue + secondValue + thirdValue == 2020)
+                            return baseValue * secondValue * thirdValue;
+                    }
+                }
+            }
+
+            return -1;
         }
     }
 }
--- Solvers/Wizards/Fabio/Fabio02.cs
+++ Solvers/Wizards/Fabio/Fabio02.cs
@@ -14,12 +14,62 @@ namespace Solvers
 
         public override long SolvePartOne(string[] input)
         {
-            throw new NotImplementedException();
+            var validPasswords = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                ParseLine(input, i, out var min, out var max, out var letter, out var password);
+
+                var numOccur = 0;
+                for (var j = 0; j < password.Length; j++)
+                    if (password[j] == letter)
+                        numOccur++;
+
+                if (numOccur > min && numOccur < max)
+                    validPasswords++;
+            }
+
+            return validPasswords;
         }
 
         public override long SolvePartTwo(string[] input)
         {
-            throw new NotImplementedException();
+            var validPasswords = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                ParseLine(input, i, out var equalIndex, out var differentIndex, out var letter, out var password);
+
+                if ((password[equalIndex - 1] == letter) ^ (password[differentIndex - 1] == letter))
+                    validPasswords++;
+            }
+
+            return validPasswords;
+        }
+
+        private static void ParseLine(string[] list, int i, out int min, out int max, out char letter,
+            out string password)
+        {
+            var a = list[i].Split('-');
+            min = ConvertToInt(a[0]);
+
+            a = a[1].Split(' ');
+            max = ConvertToInt(a[0]);
+
+            var b = a[1].Split(':');
+            letter = b[0][0];
+
+            a = a[2].Split(' ');
+            password = a[0];
+        }
+
+        private static int ConvertToInt(string s)
+        {
+            var y = 0;
+            for (var i = 0; i < s.Length; i++)
+                y = y * 10 + (s[i] - '0');
+
+            return y;
         }
     }
 }
